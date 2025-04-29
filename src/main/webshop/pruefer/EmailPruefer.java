5b9ba345ea6bc3cdba167ce6cc00c673d4ac2cd5
n0rs <n0rs9485@googlemail.com>
2025-04-29 12:52:57 +0200

    sfgasg

--- src/main/webshop/pruefer/EmailPruefer.java
+++ src/main/webshop/pruefer/EmailPruefer.java
@@ -3,16 +3,13 @@ package webshop.pruefer;
 
 public class EmailPruefer implements Pruefer {
     public boolean pruefe(String email) {
-        // Überprüfen, ob die E-Mail-Adresse ein @-Zeichen enthält
         if (!email.contains("@")) {
             return false;
         }
-        // Überprüfen, ob die E-Mail-Adresse einen Punkt nach dem @-Zeichen enthält
         String domain = email.substring(email.indexOf("@") + 1);
         if (!domain.contains(".")) {
             return false;
         }
-        // Überprüfen, ob die E-Mail-Adresse eine gültige Länge hat
         if (email.length() < 5 || email.length() > 50) {
             return false;
         }
