c8c1f43a4d44a035a291b14101e08a2ec6aa795a
lisaphamie <lisa-pham@gmx.de>
2025-05-05 18:48:22 +0200

    Passwort wird nun solange abgefragt, bis es den Anforderungen entspricht

--- src/main/webshop/Main.java
+++ src/main/webshop/Main.java
@@ -9,31 +9,34 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        boolean istGueltig = false;
+        boolean emailIstGueltig = false;
+        boolean pwIstGueltig = false;
 
-        while(!istGueltig) {
+        while(!emailIstGueltig) {
             System.out.print("Wie lautet Ihre E-Mail-Adresse: ");
             String email = scanner.nextLine();
 
             EmailPruefer emailPruefer = new EmailPruefer();
             if (emailPruefer.pruefe(email)) {
                 System.out.println("Gültige E-Mail-Adresse: " + email);
-                istGueltig = true;
+                emailIstGueltig = true;
             } else {
                 System.out.println("Ungültige E-Mail-Adresse. Bitte versuchen Sie es erneut.");
             }
         }
+        while(!pwIstGueltig) {
 
-        System.out.print("Wie lautet Ihr Passwort: ");
-        String pw = scanner.nextLine();
+            System.out.print("Wie lautet Ihr Passwort: ");
+            String pw = scanner.nextLine();
 
-        PasswortPruefer pruefer = new PasswortPruefer(); // Instanz erstellen
-        if (pruefer.pruefe(pw)) {
-            System.out.println("Gutes Passwort");
-        } else {
-            System.out.println("Schlechtes Passwort");
+            PasswortPruefer pruefer = new PasswortPruefer(); // Instanz erstellen
+            if (pruefer.pruefe(pw)) {
+                System.out.println("Gutes Passwort");
+                pwIstGueltig = true;
+            } else {
+                System.out.println("Schlechtes Passwort. Bitte versuchen Sie es erneut.");
+            }
         }
-
         scanner.close();
     }
 }
