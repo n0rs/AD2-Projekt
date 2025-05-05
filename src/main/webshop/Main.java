fa113a999eb391322d3f1ba2747bcb8a42f27e81
lisaphamie <lisa-pham@gmx.de>
2025-05-05 19:19:29 +0200

    break statt den zwei booleans

--- src/main/webshop/Main.java
+++ src/main/webshop/Main.java
@@ -9,30 +9,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        boolean emailIstGueltig = false;
-        boolean pwIstGueltig = false;
 
-        while(!emailIstGueltig) {
+        while(true) {
             System.out.print("Wie lautet Ihre E-Mail-Adresse: ");
             String email = scanner.nextLine();
 
             EmailPruefer emailPruefer = new EmailPruefer();
             if (emailPruefer.pruefe(email)) {
                 System.out.println("Gültige E-Mail-Adresse: " + email);
-                emailIstGueltig = true;
+                break;
             } else {
                 System.out.println("Ungültige E-Mail-Adresse. Bitte versuchen Sie es erneut.");
             }
         }
-        while(!pwIstGueltig) {
-
+        while(true) {
             System.out.print("Wie lautet Ihr Passwort: ");
             String pw = scanner.nextLine();
 
             PasswortPruefer pruefer = new PasswortPruefer(); // Instanz erstellen
             if (pruefer.pruefe(pw)) {
-                System.out.println("Gutes Passwort");
-                pwIstGueltig = true;
+                System.out.println("Starkes Passwort");
+                break;
             } else {
                 System.out.println("Schlechtes Passwort. Bitte versuchen Sie es erneut.");
             }
