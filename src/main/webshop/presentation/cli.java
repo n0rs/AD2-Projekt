b098ec8797459961163a93c8b22401f2fdfd5b65
n0rs <n0rs9485@googlemail.com>
2025-05-20 11:13:41 +0200

    CLI find ich doof

--- src/main/webshop/presentation/cli.java
+++ src/main/webshop/presentation/cli.java
@@ -1,5 +1,10 @@
 package webshop.presentation;
 
 public class cli {
-    
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+    public static void printError(String message) {
+        System.err.println(message);
+    }
 }
