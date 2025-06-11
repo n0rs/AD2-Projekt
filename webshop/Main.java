ce2c4e3577b7f23533f0f23edb37a5779c621ce8
lisaphamie <lisa-pham@gmx.de>
2025-06-11 22:59:11 +0200

    Kundenregistrierung und PasswortZuruecksetzen - jetziger Stand

--- webshop/Main.java
+++ webshop/Main.java
@@ -1,5 +1,5 @@
 // javac -cp "lib/*" -d ./bin ./webshop/Main.java ./webshop/businessLayer/Objekte/*.java ./webshop/businessLayer/service/*.java ./webshop/businessLayer/validation/*.java ./webshop/dataAccessLayer/*.java ./webshop/presentationLayer/*.java kompiliert Programm
-// java -cp ".\bin;lib\*" webshop.Main füht Main.java aus
+// java -cp ".\bin;lib\*" webshop.Main führt Main.java aus
 
 package webshop;
 
@@ -7,11 +7,15 @@ package webshop;
 
 // eigene Imports
 
+import webshop.businessLayer.Objekte.Kunde;
 import webshop.businessLayer.service.EmailVersand;
+import webshop.businessLayer.service.Kundenregistrierung;
+
 
 public class Main {
     public static void main(String[] args) {
-    /*try (Scanner scanner = new Scanner(System.in)) {
+
+     /*try (Scanner scanner = new Scanner(System.in)) {
         String email;
         String passwort;
         // Verbindung zur Datenbank herstellen und eine Abfrage durchführen
@@ -22,7 +26,7 @@ public class Main {
         email = EmailPruefer.starteEmailPruefung(scanner);
         passwort = PasswortPruefer.startePasswortPruefung(scanner);
 
-        Kunde kunde = new Kunde(email, passwort);
+       Kunde kunde = new Kunde(email, passwort);
         Kunde kunde2 = new Kunde("asas@123.de", "asghasgh123!*");
         DatenbankManager.kundeAnlegen(kunde.getEmail(), kunde.getPassword());
         DatenbankManager.kundeAnlegen(kunde2.getEmail(), kunde2.getPassword());
@@ -52,11 +56,14 @@ public class Main {
         System.exit(0);
     }*/
 
-    try {
-        EmailVersand.sendeEmail("muesse.christian@gmail.com", "Test", "TESTTESTTEST");
-    } catch (jakarta.mail.MessagingException e) {
-        System.out.println("Fehler beim Senden der E-Mail: " + e.getMessage());
-        e.printStackTrace();
+        Kundenregistrierung.registriereKunde(new Kunde("lisa-pham@gmx.de", "1234567890!A"));
+
+        try {
+            EmailVersand.sendeEmail("lisa-pham@gmx.de", "Test", "TESTTESTTEST");
+        } catch (jakarta.mail.MessagingException e) {
+            System.out.println("Fehler beim Senden der E-Mail: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
     }
-}
--- webshop/businessLayer/service/EmailVersand.java
+++ webshop/businessLayer/service/EmailVersand.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class EmailVersand {
+    // Absender-Adresse und Passwort werden beim Laden der Klasse initialisiert
     private static String ABSENDER;
     private static String PASSWORT;
 
@@ -23,27 +24,31 @@ public class EmailVersand {
             }
         }
 
-    public static void sendeEmail(String empfaenger, String betreff, String text) throws MessagingException {
+    public static void sendeEmail(String empfaenger, String betreff, String inhalt) throws MessagingException {
 
+        // SMTP-Konfiguration (hier für Gmail ausgelegt
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");    // Authentifizierung aktivieren
+        props.put("mail.smtp.starttls.enable", "true"); // TLS-Verschlüsselung aktivieren
+        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP-Server von Gmail
+        props.put("mail.smtp.port", "587"); // TLS-Port
 
+        // Session mit Authentifizierung erzeugen
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(ABSENDER, PASSWORT);
             }
         });
-
+        // E-Mail-Nachricht erstellen
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(ABSENDER));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(empfaenger));
         message.setSubject(betreff);
-        message.setText(text);
+        message.setContent(inhalt, "text/html; charset=utf-8");
 
+        // Nachricht senden
+        System.out.println("E-Mail wird versendet...");
         Transport.send(message);
-        System.out.println("E-Mail gesendet!");
+        System.out.println("E-Mail gesendet an " + empfaenger + "!");
     }
 }
